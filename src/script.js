f82260e7645e41adb144abb0945cd74bfd40718d
Vikms95 <vms1995@hotmail.com>
2022-10-11 03:23:04 +0200

    create vehicle path

--- src/script.js
+++ src/script.js
@@ -1,10 +1,13 @@
 import * as THREE from 'three'
+import * as YUKA from 'yuka'
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
+
+renderer.setClearColor(0XA3A3A3)
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -13,10 +16,55 @@ const camera = new THREE.PerspectiveCamera(
   1000
 )
 
-const axesHelper = new THREE.AxesHelper(5)
-scene.add(axesHelper)
+camera.position.set(0, 20, 0)
+camera.lookAt(scene.position)
 
-camera.position.z = 5
-camera.position.y = 2
+const vehicleGeometry = new THREE.ConeBufferGeometry(0.1, 0.5, 8)
+const vehicleMaterial = new THREE.MeshNormalMaterial()
+const vehicleMesh = new THREE.Mesh(vehicleGeometry, vehicleMaterial)
+vehicleMesh.matrixAutoUpdate = false
+scene.add(vehicleMesh)
+
+const vehicle = new YUKA.Vehicle()
+vehicle.setRenderComponent(vehicleMesh, sync)
+
+function sync(entity, renderComponent) {
+  renderComponent.matrix.copy(entity.worldMatrix)
+}
+
+const path = new YUKA.Path()
+
+path.add(new YUKA.Vector3(-4, 0 ,4))
+path.add(new YUKA.Vector3(-6, 0 ,0))
+path.add(new YUKA.Vector3(-4, 0 ,-4))
+path.add(new YUKA.Vector3(0, 0 ,0))
+path.add(new YUKA.Vector3(4, 0 ,-4))
+path.add(new YUKA.Vector3(6, 0 ,0))
+path.add(new YUKA.Vector3(4, 0 ,4))
+path.add(new YUKA.Vector3(0, 0 ,6))
+
+vehicle.position.copy(path.current())
+
+const followPathBehavior = new YUKA.FollowPathBehavior(path, 0.5)
+vehicle.steering.add(followPathBehavior)
+
+const entityManager = new YUKA.EntityManager()
+entityManager.add(vehicle)
+
+const time = new YUKA.Time()
+
+function animate() {
+  const delta = time.update().getDelta()
+  entityManager.update(delta)
+  renderer.render(scene, camera)
+}
+
+renderer.setAnimationLoop(animate)
+
+window.addEventListener('resize', function() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
 
 renderer.render(scene,camera)
