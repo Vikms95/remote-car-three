903a085688e7595051b00172191ea01510ef177d
Vikms95 <vms1995@hotmail.com>
2022-10-11 05:39:30 +0200

    replace general imports to avoid incompatibility with parcel

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-import * as THREE from 'three';
-import * as YUKA from 'yuka';
+import {
+  WebGLRenderer,
+  Scene,
+  PerspectiveCamera,
+  AmbientLight,
+  DirectionalLight,
+  ConeBufferGeometry,
+  MeshNormalMaterial,
+  Mesh,
+  Vector2,
+  PlaneGeometry,
+  MeshBasicMaterial,
+  Raycaster
+} from 'three';
+import {Vehicle, EntityManager, GameEntity, ArriveBehavior, Time} from 'yuka';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new WebGLRenderer({antialias: true});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -10,11 +23,11 @@ document.body.appendChild(renderer.domElement);
 
 const carURL = new URL('./static/SUV.glb', import.meta.url)
 
-const scene = new THREE.Scene();
+const scene = new Scene();
 
 renderer.setClearColor(0xA3A3A3);
 
-const camera = new THREE.PerspectiveCamera(
+const camera = new PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
     0.1,
@@ -24,20 +37,20 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(0, 10, 4);
 camera.lookAt(scene.position);
 
-const ambientLight = new THREE.AmbientLight(0x333333);
+const ambientLight = new AmbientLight(0x333333);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+const directionalLight = new DirectionalLight(0xFFFFFF, 1);
 scene.add(directionalLight);
 
-const vehicleGeometry = new THREE.ConeBufferGeometry(0.1, 0.5, 8);
+const vehicleGeometry = new ConeBufferGeometry(0.1, 0.5, 8);
 vehicleGeometry.rotateX(Math.PI * 0.5);
-const vehicleMaterial = new THREE.MeshNormalMaterial();
-const vehicleMesh = new THREE.Mesh(vehicleGeometry, vehicleMaterial);
+const vehicleMaterial = new MeshNormalMaterial();
+const vehicleMesh = new Mesh(vehicleGeometry, vehicleMaterial);
 vehicleMesh.matrixAutoUpdate = false;
 scene.add(vehicleMesh);
 
-const vehicle = new YUKA.Vehicle();
+const vehicle = new Vehicle();
 
 vehicle.scale.set(0.15, 0.15, 0.15)
 
@@ -45,7 +58,7 @@ function sync(entity, renderComponent) {
     renderComponent.matrix.copy(entity.worldMatrix);
 }
 
-const entityManager = new YUKA.EntityManager();
+const entityManager = new EntityManager();
 entityManager.add(vehicle);
 
 const loader = new GLTFLoader()
@@ -58,29 +71,29 @@ loader.load(carURL.href, function(glb) {
 
 
 
-const target = new YUKA.GameEntity();
+const target = new GameEntity();
 entityManager.add(target);
 
-const seekBehavior = new YUKA.ArriveBehavior(target.position, 3, 0.5);
+const seekBehavior = new ArriveBehavior(target.position, 3, 0.5);
 vehicle.steering.add(seekBehavior);
 
 vehicle.position.set(-2, 0, -2);
 
-const mousePosition = new THREE.Vector2()
+const mousePosition = new Vector2()
 
 window.addEventListener('mousemove', function(e) {
   mousePosition.x = (e.clientX / this.window.innerWidth) * 2 -1
   mousePosition.y = -(e.clientY / this.window.innerHeight) * 2 + 1
 })
 
-const planeGeo = new THREE.PlaneGeometry(25, 25)
-const planeMat = new THREE.MeshBasicMaterial({visible: false})
-const planeMesh = new THREE.Mesh(planeGeo, planeMat)
+const planeGeo = new PlaneGeometry(25, 25)
+const planeMat = new MeshBasicMaterial({visible: false})
+const planeMesh = new Mesh(planeGeo, planeMat)
 planeMesh.rotation.x = -0.5 * Math.PI
 scene.add(planeMesh)
 planeMesh.name = 'plane'
 
-const raycaster = new THREE.Raycaster()
+const raycaster = new Raycaster()
 
 window.addEventListener('click', function() {
   raycaster.setFromCamera(mousePosition, camera)
@@ -91,7 +104,7 @@ window.addEventListener('click', function() {
   }
 })
 
-const time = new YUKA.Time();
+const time = new Time();
 
 function animate() {
     const delta = time.update().getDelta();
